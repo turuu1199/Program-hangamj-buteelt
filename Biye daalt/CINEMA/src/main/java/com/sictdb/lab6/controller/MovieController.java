448e45e63f31cdcc231335f631502d19acbe4646
Turmandakh <85722584+turuu1199@users.noreply.github.com>
2023-12-09 20:28:43 +0800

    final

--- Biye daalt/CINEMA/src/main/java/com/sictdb/lab6/controller/MovieController.java
+++ Biye daalt/CINEMA/src/main/java/com/sictdb/lab6/controller/MovieController.java
@@ -5,26 +5,32 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.sictdb.lab6.model.Movie;
 import com.sictdb.lab6.repository.MovieRepository;
+import com.sictdb.lab6.services.MovieService;
+
 
 @Controller
 public class MovieController {
 
     @Autowired
-    private MovieRepository repository;
+    private MovieService service;
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+    	List<Movie> movies = service.getAllMovies();
+  	    model.addAttribute("movies", movies);
         return "home";
     }
 
@@ -34,7 +40,7 @@ public class MovieController {
                               @RequestParam String movieName) {
         System.out.println(img.getOriginalFilename());
         Movie newMovie = new Movie(movieName, movieDesc, img.getOriginalFilename());
-        repository.save(newMovie);
+        service.saveMovie(newMovie);
 
         if (newMovie != null) {
             try {
@@ -49,7 +55,45 @@ public class MovieController {
         return "redirect:/";
     }
     @GetMapping("/all-movies")
-    public String getMovies(Model model) {
-    	return "all-movies";
+	public String listUsers(Model model) {
+	    List<Movie> movies = service.getAllMovies();
+	    model.addAttribute("movies", movies);
+	    return "all-movies";
+	}
+    @GetMapping("/add-movie")
+    public String home() {
+        return "add-movie";
     }
+    @PostMapping("/delete/{id}")
+    public String deleteMovie(@PathVariable long id) {
+    	service.deleteMovieById(id);
+    	return "redirect:/all-movies";
+    }
+    @PostMapping("/update/{id}")
+    public String updateMovie(@RequestParam long id,
+    						  @RequestParam MultipartFile img,
+                              @RequestParam String movieDesc,
+                              @RequestParam String movieName) {
+    	Movie updateMovie = new Movie(movieName,movieDesc,img.getOriginalFilename());
+    	updateMovie.setId(id);
+    	service.saveMovie(updateMovie);
+    	return "redirect:/all-movies";
+    }
+    @GetMapping("/update/{id}")
+    public String updateMovie(@PathVariable long id, Model model) {
+    	Movie movie = service.getMovieById(id);
+    	model.addAttribute("movie",movie);
+    	return "update-movie";
+    }
+    @GetMapping("/{id}")
+    public String getMovie(Model model,@PathVariable long id) {
+    	Movie movie = service.getMovieById(id);
+    	model.addAttribute("movie",movie);
+    	return "view-movie";
+    }
+    @GetMapping("/about-me")
+    public String aboutMe() {
+    	return "about-me";
+    }
+
 }
